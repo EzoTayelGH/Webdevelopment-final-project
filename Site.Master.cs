749f794a12f8f2abeb43ed62a899411bcd784bb3
EzoTayelGH <enadtl@gmail.com>
2025-12-29 14:19:54 +0200

    First version frontend
    this version is before usin the framework and the framwrok version will be added on 30/dec 2025

---
+++ Site.Master.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace MasculineLifeASPNET
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Debug: Check if Header exists
+            if (Page.Header == null)
+            {
+                // Try to find it manually
+                var head = FindControl("head1") as HtmlHead;
+                if (head != null)
+                {
+                    AddCssLink(head);
+                }
+                else
+                {
+                    // Log error
+                    System.Diagnostics.Debug.WriteLine("Header not found!");
+                }
+            }
+            else
+            {
+                AddCssLink(Page.Header);
+            }
+        }
+
+        private void AddCssLink(HtmlHead head)
+        {
+            HtmlLink cssLink = new HtmlLink();
+            cssLink.Href = ResolveUrl("~/Content/styles.css");
+            cssLink.Attributes["rel"] = "stylesheet";
+            cssLink.Attributes["type"] = "text/css";
+            head.Controls.Add(cssLink);
+
+            // Debug output
+            System.Diagnostics.Debug.WriteLine($"CSS Added: {cssLink.Href}");
+        }
+    }
+}
